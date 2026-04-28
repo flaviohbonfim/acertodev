333af2edb2cf6857a20c2d936f1ecc21b12e1401
Flávio Henrique Bonfim <flaviohbonfim@live.com>
2026-04-28 13:14:28 -0300

    refactor: remove unused Button import in PaginationControls component

--- components/PaginationControls.tsx
+++ components/PaginationControls.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { HStack, Button, Text, IconButton, useColorModeValue } from '@chakra-ui/react';
+import { HStack, Text, IconButton, useColorModeValue } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 interface PaginationControlsProps {
